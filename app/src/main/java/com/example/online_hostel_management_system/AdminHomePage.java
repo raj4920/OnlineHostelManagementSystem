d1534efde4c4fe81556450b9bc2cf5ddc4ad3be3
Raj <rajzalavadiya2000@gmail.com>
2021-10-15 21:44:07 +0530

    Fist commit Creation of All for One App

--- app/src/main/java/com/example/online_hostel_management_system/AdminHomePage.java
+++ app/src/main/java/com/example/online_hostel_management_system/AdminHomePage.java
@@ -2,13 +2,34 @@ package com.example.online_hostel_management_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminHomePage extends AppCompatActivity {
+
+    Button btnmrd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_page);
+    }
+
+    private void controlIns()
+    {
+        btnmrd=findViewById(R.id.btnmrd);
+    }
+
+    private void eventHandler()
+    {
+        btnmrd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it1=new Intent(AdminHomePage.this,MaintainRoomDetails.class);
+                startActivity(it1);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/online_hostel_management_system/MaintainRoomDetails.java
+++ app/src/main/java/com/example/online_hostel_management_system/MaintainRoomDetails.java
@@ -3,6 +3,8 @@ package com.example.online_hostel_management_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MaintainRoomDetails extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MaintainRoomDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintain_room_details);
+    }
+    private  void controlIns()
+    {
+
+    }
+
+    private void eventHandler()
+    {
+
     }
 }
