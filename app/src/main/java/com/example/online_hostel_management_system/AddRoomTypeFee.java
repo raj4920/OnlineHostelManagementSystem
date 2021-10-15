e1c5030d6a3811f8ff350de0e547a363267b5a26
Raj <rajzalavadiya2000@gmail.com>
2021-10-15 22:37:26 +0530

    Fist commit Creation of All for One App

---
+++ app/src/main/java/com/example/online_hostel_management_system/AddRoomTypeFee.java
@@ -0,0 +1,14 @@
+package com.example.online_hostel_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRoomTypeFee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_room_type_fee);
+    }
+}
---
+++ app/src/main/java/com/example/online_hostel_management_system/RoomAllocationRequest.java
@@ -0,0 +1,14 @@
+package com.example.online_hostel_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomAllocationRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_allocation_request);
+    }
+}
