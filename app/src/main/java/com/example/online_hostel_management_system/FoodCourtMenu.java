62ee4b257eb14b2e0fd540f8f25b0d77afa8720a
Raj <rajzalavadiya2000@gmail.com>
2021-10-15 22:15:28 +0530

    Fist commit Creation of All for One App

---
+++ app/src/main/java/com/example/online_hostel_management_system/FoodCourtMenu.java
@@ -0,0 +1,14 @@
+package com.example.online_hostel_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodCourtMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_court_menu);
+    }
+}
