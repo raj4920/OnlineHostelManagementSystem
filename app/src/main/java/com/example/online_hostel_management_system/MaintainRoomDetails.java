613421e048877c041b8b6998b25dd96167dbc027
Raj <rajzalavadiya2000@gmail.com>
2021-10-15 18:10:58 +0530

    Fist commit Creation of All for One App

---
+++ app/src/main/java/com/example/online_hostel_management_system/MaintainRoomDetails.java
@@ -0,0 +1,14 @@
+package com.example.online_hostel_management_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaintainRoomDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintain_room_details);
+    }
+}
