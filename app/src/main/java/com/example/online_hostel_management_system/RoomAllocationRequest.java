4f6b199efb7006e58276ce91cda6da72475f7a13
Raj <rajzalavadiya2000@gmail.com>
2021-10-16 09:13:19 +0530

    Fist commit Creation of All for One App

--- app/src/main/java/com/example/online_hostel_management_system/RoomAllocationRequest.java
+++ app/src/main/java/com/example/online_hostel_management_system/RoomAllocationRequest.java
@@ -1,14 +1,23 @@
 package com.example.online_hostel_management_system;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.hardware.camera2.params.BlackLevelPattern;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Collections;
 
 public class RoomAllocationRequest extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_allocation_request);
+
     }
 }
