515e1012c51a5837c90df63b821fa8ac95a56934
Raj <rajzalavadiya2000@gmail.com>
2021-10-16 11:19:44 +0530

    Fist commit Creation of All for One App

--- app/src/main/java/com/example/online_hostel_management_system/AdminHomePage.java
+++ app/src/main/java/com/example/online_hostel_management_system/AdminHomePage.java
@@ -9,26 +9,88 @@ import android.widget.Button;
 
 public class AdminHomePage extends AppCompatActivity {
 
-    Button btnmrd;
+    Button btnViewRoomAllocationRequest;
+    Button btnMaintainRoomDetails;
+    Button btnViewStudentComplaint;
+    Button btnSetFoodCourtMenu;
+    Button btnViewStudentFeeStatus;
+    Button btnMaintainHostelExpence;
+    Button btnGenerateReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_page);
+        controlIns();
+        eventHandler();
     }
 
     private void controlIns()
     {
-        btnmrd=findViewById(R.id.btnmrd);
+        btnViewRoomAllocationRequest=findViewById(R.id.btnvsrar);
+        btnMaintainRoomDetails=findViewById(R.id.btnmrd);
+        btnViewStudentComplaint=findViewById(R.id.btnvscm);
+        btnSetFoodCourtMenu=findViewById(R.id.btnufcm);
+        btnViewStudentFeeStatus=findViewById(R.id.btnvsfs);
+        btnMaintainHostelExpence=findViewById(R.id.btnmhe);
+        btnGenerateReport=findViewById(R.id.btngr);
     }
 
     private void eventHandler()
     {
-        btnmrd.setOnClickListener(new View.OnClickListener() {
+        btnViewRoomAllocationRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent it1=new Intent(AdminHomePage.this,MaintainRoomDetails.class);
+                Intent it1=new Intent();
                 startActivity(it1);
+            }
+        });
+
+        btnMaintainRoomDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it2=new Intent(AdminHomePage.this,MaintainRoomDetails.class);
+                startActivity(it2);
+            }
+        });
+
+        btnViewStudentComplaint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it3=new Intent();
+                startActivity(it3);
+            }
+        });
+
+        btnSetFoodCourtMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it4=new Intent(AdminHomePage.this,FoodCourtMenu.class);
+                startActivity(it4);
+            }
+        });
+
+        btnViewStudentFeeStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it5=new Intent();
+                startActivity(it5);
+            }
+        });
+
+        btnMaintainHostelExpence.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it6=new Intent(AdminHomePage.this,MaintainHostelExpense.class);
+                startActivity(it6);
+            }
+        });
+
+        btnGenerateReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it7=new Intent();
+                startActivity(it7);
             }
         });
     }
