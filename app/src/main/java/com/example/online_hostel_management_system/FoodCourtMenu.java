4ee35cc42e265739e76eaa875fb3e4db71e18302
Raj <rajzalavadiya2000@gmail.com>
2021-10-17 22:16:52 +0530

    Fist commit Creation of All for One App

--- app/src/main/java/com/example/online_hostel_management_system/FoodCourtMenu.java
+++ app/src/main/java/com/example/online_hostel_management_system/FoodCourtMenu.java
@@ -5,17 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.accessibility.AccessibilityManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class FoodCourtMenu extends AppCompatActivity{
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class FoodCourtMenu extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Spinner Day;
     Spinner Time;
-
+    EditText dishName;
+    Button btnSubmit;
     String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
     String[] time={"Breakfast","Lunch","Dinner"};
+
+    String day="";
+    String ftime="";
+
+    String URL="http://192.168.56.1/OHMS/AddFoodCourtMenu.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +49,94 @@ public class FoodCourtMenu extends AppCompatActivity{
     {
         Day=findViewById(R.id.day);
         Time=findViewById(R.id.time);
-    }
+        dishName=findViewById(R.id.dn);
+        btnSubmit=findViewById(R.id.btnsm);
 
-    private void eventHandler()
-    {
         ArrayAdapter aa1=new ArrayAdapter(this, R.layout.days_layout,days);
         aa1.setDropDownViewResource(R.layout.days_layout);
         Day.setAdapter(aa1);
+        Day.setOnItemSelectedListener(this);
+
 
         ArrayAdapter aa2=new ArrayAdapter(this,R.layout.days_layout,time);
         aa2.setDropDownViewResource(R.layout.days_layout);
         Time.setAdapter(aa2);
+        Time.setOnItemSelectedListener(this);
 
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        day=adapterView.getItemAtPosition(i).toString();
+        ftime=adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private void eventHandler()
+    {
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String DishName=dishName.getText().toString();
+
+                boolean check=validation(DishName);
+
+                if(check==true)
+                {
+                    addFoodMenu();
+                    Toast.makeText(getApplicationContext(),"Done",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Error...",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    private boolean validation(String DishName)
+    {
+        if(DishName.length()==0)
+        {
+            dishName.requestFocus();
+            dishName.setError("Field cannot be empty");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void addFoodMenu()
+    {
+        StringRequest addRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        })
+        {
+            protected Map<String,String> getParams() throws AuthFailureError{
+                Map<String,String> params=new HashMap<String,String>();
+                params.put("dishName",dishName.getText().toString());
+                params.put("day",day);
+                params.put("time",ftime);
+                return params;
+            }
+        };
+        RequestQueue queue= Volley.newRequestQueue(this);
+        queue.add(addRequest);
     }
 }
--- app/src/main/java/com/example/online_hostel_management_system/ViewFoodCourtMenu.java
+++ app/src/main/java/com/example/online_hostel_management_system/ViewFoodCourtMenu.java
@@ -3,9 +3,11 @@ package com.example.online_hostel_management_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ViewFoodCourtMenu extends AppCompatActivity {
 
+    ListView lst;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
