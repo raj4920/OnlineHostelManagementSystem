877cd20aa6a3341aaa76ca9e92921655d31ee52b
Raj <rajzalavadiya2000@gmail.com>
2021-10-19 03:41:15 +0530

    Fist commit Creation of All for One App

--- app/src/main/java/com/example/online_hostel_management_system/StaffHomePage.java
+++ app/src/main/java/com/example/online_hostel_management_system/StaffHomePage.java
@@ -1,15 +1,45 @@
 package com.example.online_hostel_management_system;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationRequest;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StaffHomePage extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
+public class StaffHomePage extends AppCompatActivity implements LocationListener{
 
     Button btnEntryAttendance;
     Button btnExitAttendance;
+
+    double latitude;
+    double longitude;
+
+    TextView txtLocation;
+
+    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,19 +48,51 @@ public class StaffHomePage extends AppCompatActivity {
         eventHandler();
     }
 
-    private void controlIns()
-    {
-        btnEntryAttendance=findViewById(R.id.btnena);
-        btnExitAttendance=findViewById(R.id.btnexa);
+    private void controlIns() {
+        btnEntryAttendance = findViewById(R.id.btnena);
+        btnExitAttendance = findViewById(R.id.btnexa);
+        txtLocation=findViewById(R.id.tvd);
     }
 
-    private void eventHandler()
-    {
+    private void eventHandler() {
         btnEntryAttendance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                getLocation();
             }
         });
+    }
+
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, (LocationListener) this);
+        }catch (SecurityException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude=location.getLatitude();
+        longitude=location.getLongitude();
+
+        txtLocation.setText("Current Location = " + location.getLatitude() + "," + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(StaffHomePage.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
     }
 }
