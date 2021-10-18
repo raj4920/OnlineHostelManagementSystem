fc6525d3c2a85b4564231e2ab25303576e107cee
Raj <rajzalavadiya2000@gmail.com>
2021-10-18 13:40:04 +0530

    Fist commit Creation of All for One App

--- app/src/main/java/com/example/online_hostel_management_system/ForgetPassword.java
+++ app/src/main/java/com/example/online_hostel_management_system/ForgetPassword.java
@@ -2,6 +2,104 @@ package com.example.online_hostel_management_system;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ForgetPassword extends AppCompatActivity {
+
+    String url1="http://192.168.56.1/OHMS/ForgetPassword.php";
+    EditText txtemailid;
+    Button btnpassword;
+    String email;
+    int send=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+
+        txtemailid=findViewById(R.id.email);
+        btnpassword=findViewById(R.id.btnsp);
+
+        btnpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                email=txtemailid.getText().toString();
+                send=0;
+
+                if(TextUtils.isEmpty(email))
+                {
+                    txtemailid.setError("Enter Email ID");
+                    txtemailid.requestFocus();
+                    return;
+                }
+
+
+                forgetPassword();
+                Intent intentForget=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intentForget);
+                //Toast.makeText(getApplicationContext(),"Password will be send in your mail",Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void forgetPassword()
+    {
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, url1, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                if(response.equals("Invalid Email ID"))
+                {
+                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params=new HashMap<>();
+                params.put("emailid",email);
+                return  params;
+            }
+
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+    }
+}
+/*import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class ForgetPassword extends AppCompatActivity {
@@ -11,4 +109,4 @@ public class ForgetPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
     }
-}
+}*/
--- app/src/main/java/com/example/online_hostel_management_system/MainActivity.java
+++ app/src/main/java/com/example/online_hostel_management_system/MainActivity.java
@@ -1,6 +1,163 @@
 package com.example.online_hostel_management_system;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView forgetpassword,signup;
+    EditText txtemail,txtpass;
+    Button btnLogin;
+    String email,pass;
+    int role;
+    String status;
+    // String url="http://localhost/OnlineHostelManagementSystem/index.php";
+    String url="http://192.168.56.1/OHMS/Login.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        controlIns();
+        eventdHandler();
+        // loginRequest();
+
+    }
+
+    private void controlIns()
+    {
+        forgetpassword=findViewById(R.id.forgotPassword);
+        signup=findViewById(R.id.textViewSignUp);
+        btnLogin=findViewById(R.id.btnlogin);
+        txtpass=findViewById(R.id.inputPassword);
+        txtemail=findViewById(R.id.inputEmail);
+
+
+    }
+
+    public void eventdHandler()
+    {
+        forgetpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it1=new Intent(getApplicationContext(),ForgetPassword.class);
+                startActivity(it1);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it2=new Intent(getApplicationContext(),RegistrationForm.class);
+                startActivity(it2);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginRequest();
+            }
+        });
+    }
+
+    public void loginRequest()
+    {
+        //Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+        email=txtemail.getText().toString();
+        pass=txtpass.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            txtemail.setError("Please Enter Username");
+            txtemail.requestFocus();
+            return;
+        }
+        if(TextUtils.isEmpty(pass))
+        {
+            txtpass.setError("Please Enter Password");
+            txtpass.requestFocus();
+            return;
+        }
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    //Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+                    JSONObject jsonObject=new JSONObject(response);
+                    JSONArray array=jsonObject.getJSONArray("data");
+                    for (int i=0; i<array.length(); i++ ){
+                        JSONObject ob=array.getJSONObject(i);
+                        role=ob.getInt("Role");
+                        if (role == 1) {
+                            Intent intentAdmin = new Intent(MainActivity.this, AdminHomePage.class);
+                            startActivity(intentAdmin);
+                        } else if (role == 2) {
+                            Intent intentStaff = new Intent(MainActivity.this, StaffHomePage.class);
+                            startActivity(intentStaff);
+                        } else if (role == 3) {
+                            Intent intentStudent = new Intent(MainActivity.this, StudentHomePage.class);
+                            startActivity(intentStudent);
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params=new HashMap<>();
+                params.put("emailid",email);
+                params.put("password",pass);
+                return  params;
+            }
+
+        };
+        RequestQueue requestQueue=Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+
+        //  Volley.newRequestQueue(getApplicationContext()).add(stringRequest);
+    }
+}
+/*import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -84,5 +241,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-}
+}*/
